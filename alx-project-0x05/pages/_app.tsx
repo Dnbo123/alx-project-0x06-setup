12c990b8ac1dd7cf9c39d9611455936157db02fd
Don Bosco <don965877@gmail.com>
2025-01-16 03:11:37 +0300

    refactord code

--- alx-project-0x05/pages/_app.tsx
+++ alx-project-0x05/pages/_app.tsx
@@ -4,13 +4,19 @@ import type { AppProps } from "next/app";
 import { CountProvider } from "@/context/CountContext";
 
 export default function App({ Component, pageProps }: AppProps) {
+  if (!Component) {
+    throw new Error("Missing Component prop in App");
+  }
+
+  if (!pageProps) {
+    throw new Error("Missing pageProps prop in App");
+  }
+
   return (
-
-
     <CountProvider>
       <Layout>
         <Component {...pageProps} />
       </Layout>
     </CountProvider>
-  )
+  );
 }
